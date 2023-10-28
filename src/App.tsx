67ca3dbeb1f019733eb3b3dd69e7bbc868c660fc
Wanderson Barbosa <wandersonmanto@gmail.com>
2023-10-28 15:55:05 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import { EstiloGlobal } from './styles'
 
-import './global.css'
+// import './global.css'
 
 function App() {
   return (
     <>
+      <EstiloGlobal />
       <Header />
       <Hero />
       <div className="container">
--- src/components/Cabecalho/style.ts
+++ src/components/Cabecalho/style.ts
@@ -1,6 +1,8 @@
-.cabecalho {
+import styled from 'styled-components'
+
+export const Header = styled.div`
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
   text-align: center;
   padding: 24px 0;
-}
+`
